8b5fb8cf8d5b89af155136fdc9abf61805eb1be1
Adam Murray <adamm@cypress.io>
2022-06-06 09:16:32 -0500

    add failing test to see results

--- react-vite-ts/src/components/Button.cy.tsx
+++ react-vite-ts/src/components/Button.cy.tsx
@@ -3,7 +3,7 @@ import Button from './Button';
 describe('Button', () => {
   it('should mount', () => {
     cy.mount(<Button>Click Me</Button>);
-    cy.get('button').contains('Click Me');
+    cy.get('button').contains('Click Moo!');
   });
 
   it('when button is clicked, should call onClick', () => {
